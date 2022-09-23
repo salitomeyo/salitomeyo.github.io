7755a3d15bba4283c724de00ed02e7bee23a40ee
Salome Aristizabal Giraldo <salitomeyo16@gmail.com>
2022-09-23 12:11:11 -0500

    feat: animated blob outline

--- Salitomeyo-portfolio/src/pages/Home/index.tsx
+++ Salitomeyo-portfolio/src/pages/Home/index.tsx
@@ -4,20 +4,22 @@ import { Link } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import AnimatedLetters from '../../components/AnimatedLetters';
 import AnimatedBlob from '../../components/AnimatedBlob';
+import AnimatedBlobOutline from '../../components/AnimatedBlobOutline';
 
 export const Home = () => {
     const [letterClass, setLetterClass] = useState('text-animate');
     const nameArray = ['S', 'a', 'l', 'i', 't', 'o', 'm', 'e', 'y', 'o'];
-    const jobArray = ['W', 'e', 'b', ' ', 'D', 'e', 'v', 'e', 'l', 'o', 'p', 'e', 'r']; 
 
     useEffect(() => {
         setTimeout(() => {
             setLetterClass('text-animate-hover')
-        }, 3000)
+        }, 2000)
     }, [])
 
     return(
         <div className="container">
+            <AnimatedBlobOutline blobClass={"blob-container"}/>
+                <AnimatedBlobOutline blobClass={"blob-glow-outline"}/>
             <div>
                 <h1>
                     Hi <br /> I'm <span className='name'><AnimatedLetters 
@@ -25,11 +27,7 @@ export const Home = () => {
                         strArray={nameArray}
                         idx={1}
                     /></span><br />
-                    <AnimatedLetters 
-                        letterClass={letterClass}
-                        strArray={jobArray}
-                        idx={10}
-                    />
+                    Web Developer
                 </h1>
                 <h2>Frontend Developer / Game Developer / Mobile Developer </h2>
 
@@ -38,9 +36,9 @@ export const Home = () => {
                 </Link>
                 </div>
             <div>
-                <AnimatedBlob blobClass={"blob-container"}/>
-                <AnimatedBlob blobClass={"blob-glow"}/>
-                <img src={Logo} alt="" />
+                {/* <AnimatedBlob blobClass={"blob-container"}/>
+                <AnimatedBlob blobClass={"blob-glow"}/> */}
+                <img src={Logo} alt="logo" />
             </div>
         </ div>
 
